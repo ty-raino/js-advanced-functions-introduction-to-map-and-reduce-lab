833b75b210496b717ce3d85861b0684e160568a5
github username <github email address>
2021-04-10 01:38:42 -0500

    commit tree f2dc0bdf2545a33d1c2f0e506aff3238ba1e05b2

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map( a => a * -1 )
+};
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map( a => a )
+};
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map ( a => a * 2 )
+};
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map( a => a * a)
+};
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    const reducer = function(accumulator, currentValue) {
+        return accumulator + currentValue
+    };
+    return sourceArray.reduce(reducer, startingPoint)
+};
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+}
